0ee073ff9f45952dc8e1b4608a14fa9b182c54e8
SofaaSogo-oh <daniil.naumihin@yandex.ru>
2024-12-28 22:53:10 +0300

    test

--- config/utils/vimtex.nix
+++ config/utils/vimtex.nix
@@ -1,12 +1,15 @@
-{
+{pkgs, ...}: {
     plugins.vimtex = {
         enable = true;
         texlivePackage = null;
         settings = {
-                compiler_method = "latexrun";
-                compiler = "xelatex";
-                compiler_latexmk_options = [ "-shell-escape" ];
+                # compiler_method = "latexrun";
+                # compiler = "xelatex";
+                compiler_latexmk = { 
+                  options = [ "-shell-escape" ]; 
+                };
                 view_method = "zathura";
         };
+        zathuraPackage = pkgs.zathura;
     };
 }
