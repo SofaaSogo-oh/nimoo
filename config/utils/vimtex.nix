a3d75f5c7f3e88e7512d8348b3723b63555e9310
SofaaSogo-oh <daniil.naumihin@yandex.ru>
2024-12-28 17:14:52 +0300

    shell escape

--- config/utils/vimtex.nix
+++ config/utils/vimtex.nix
@@ -3,7 +3,9 @@
         enable = true;
         texlivePackage = null;
         settings = {
-                # compiler_method = "xelatex";
+                compiler_method = "latexrun";
+                compiler = "xelatex";
+                compiler_latexmk_options = [ "-shell-escape" ];
                 view_method = "zathura";
         };
     };
